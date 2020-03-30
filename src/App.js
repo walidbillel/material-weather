39ba9ea0658ef32e9effe3a985fbe9c99fa73afa
willou <walid.boubguira@gmail.com>
2020-03-30 11:45:03 -0400

    env issue

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Forcast from './components/Forcast';
+import CurrentLocation from './components/CurrentLocation'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    
+  },
+  toolBar: {
+    background: 'darkblue'
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+    letterSpacing: theme.spacing(0.4),
+    fontWeight: 'bold'
+
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolBar}>
+          <Typography variant="h5" className={classes.title}>
+            Weather App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <CurrentLocation/>
+    <Forcast/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CurrentLocation.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    margin: theme.spacing(6)
+  },
+  currentWeather: {
+    padding: theme.spacing(2),
+    marginTop: theme.spacing(4),
+    background: "darkorange",
+    color: "white",
+    textAlign: "center"
+  }
+}));
+
+const CurrentWeather = () => {
+  const classes = useStyles();
+
+  const [currentLocation, setCurrentLocation] = useState({});
+  async function fetchLocation() {
+    const result = await fetch("http://ip-api.com/json");
+    result
+      .json()
+      .then(data => {
+        setCurrentLocation(data);
+        console.log(data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    fetchLocation();
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.currentWeather} elevation={5}>
+        <Typography variant="h5">
+          Current Location: {currentLocation.city}
+        </Typography>
+      </Paper>
+    </div>
+  );
+};
+
+export default CurrentWeather;
